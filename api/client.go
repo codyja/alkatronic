12bea1344a6837c342d83b8b12f8b50e46cc080c
Cody Jarrett <codyja@gmail.com>
2021-02-28 13:27:42 -0600

    first commit

---
+++ api/client.go
@@ -0,0 +1,216 @@
+package api
+
+import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"net/url"
+	"strings"
+	"sync"
+)
+
+// Client is the API client for making calls to alkatronic's api
+type Client interface {
+	BaseURL() *url.URL
+	AccessToken() string
+	Authenticate(username, password string) error
+}
+
+// AlkatronicClient is the client implementation that calls Alkatronic
+type AlkatronicClient struct {
+	c           *http.Client
+	baseURL     *url.URL
+	accessToken string
+	m           sync.Mutex
+}
+
+// NewAlkatronicClient returns a new Client
+func NewAlkatronicClient(baseURL string) (*AlkatronicClient, error) {
+	parsedURL, err := url.Parse(baseURL)
+	if err != nil {
+		return nil, fmt.Errorf("Unable to parse baseURL: %w", err)
+	}
+
+	h := &http.Client{}
+	c := &AlkatronicClient{
+		c:       h,
+		baseURL: parsedURL,
+	}
+
+	return c, nil
+}
+
+// BaseURL returns the Client's configured API URL
+func (c *AlkatronicClient) BaseURL() *url.URL {
+	u, _ := url.Parse(c.baseURL.String())
+	return u
+}
+
+// AccessToken returns the API Access Token stored in the Client
+func (c *AlkatronicClient) AccessToken() string {
+	return c.accessToken
+}
+
+// SetAccessToken sets c.accessToken to use an existing token read from home directory
+func (c *AlkatronicClient) SetAccessToken(token string) {
+	c.accessToken = token
+}
+
+// Do executes an HTTP request with the Client's HTTP client.
+// Do automatically adds the Access Token to the request
+func (c *AlkatronicClient) Do(req *http.Request) (*http.Response, error) {
+	if c.accessToken != "" {
+		req.Header.Set("token", c.accessToken)
+		req.Header.Set("User-Agent", "Alkatronic_GO_API/1.0")
+	}
+
+	return c.c.Do(req)
+}
+
+// DoRequest executes an HTTP request
+//func (c *AlkatronicClient) DoWithExpectedStatus(req *http.Request, status int) (*Response, error) {
+func (c *AlkatronicClient) DoRequest(req *http.Request) (*http.Response, error) {
+	resp, err := c.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	var bodyBytes []byte
+	if resp.Body != nil {
+		bodyBytes, _ = ioutil.ReadAll(resp.Body)
+		resp.Body.Close()
+	}
+	resp.Body = ioutil.NopCloser(bytes.NewBuffer(bodyBytes))
+
+	var r Response
+	_ = json.NewDecoder(resp.Body).Decode(&r)
+
+	resp.Body.Close()
+	resp.Body = ioutil.NopCloser(bytes.NewBuffer(bodyBytes))
+
+	if r.Result != true {
+		err = errors.New(r.Message)
+		return nil, err
+	}
+
+	return resp, nil
+}
+
+// Authenticate uses the provided username and password to auth against the Alkatronic site API
+// and retrieves a single token. The token is stored internal to the Client. The username/password
+// are encoded and sent as form data.
+func (c *AlkatronicClient) Authenticate(username, password string) {
+	p, _ := url.Parse("/users/login")
+	b := url.Values{
+		"email":    {username},
+		"password": {password},
+	}
+	req, err := http.NewRequest(http.MethodPost, c.baseURL.ResolveReference(p).String(), strings.NewReader(b.Encode()))
+	if err != nil {
+		fmt.Errorf("error building token generate request: %w", err)
+	}
+
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+
+	log.Printf("Starting authentication against Alkatronic's API using username and password")
+	resp, err := c.DoRequest(req)
+	if err != nil {
+		log.Fatalf("Authentication failed: %s", err)
+		return
+	}
+
+	var r Response
+	_ = json.NewDecoder(resp.Body).Decode(&r)
+	c.accessToken = r.Data
+
+	log.Printf("Authentication has succeeded")
+
+	return
+}
+
+func (c *AlkatronicClient) GetDevices() (*Devices, error) {
+	p, _ := url.Parse("/users/devices")
+
+	req, err := http.NewRequest(http.MethodGet, c.baseURL.ResolveReference(p).String(), nil)
+	if err != nil {
+		fmt.Println("must be an errors at req")
+		return nil, err
+	}
+	// Add token to query string to get devices
+	q := req.URL.Query()
+	q.Add("token", c.accessToken)
+	req.URL.RawQuery = q.Encode()
+
+	resp, err := c.DoRequest(req)
+	if err != nil {
+		return nil, fmt.Errorf("error making http call: %w", err)
+	}
+
+	var d *Devices
+	err = json.NewDecoder(resp.Body).Decode(&d)
+	if err != nil {
+		return nil, fmt.Errorf("GetDevices error decoding response: %w", err)
+	}
+
+	return d, err
+}
+
+func (c *AlkatronicClient) GetRecords(deviceID int, days int) (*Records, error) {
+	p, _ := url.Parse(fmt.Sprintf("/users/devices/%d/records/%d", deviceID, days))
+
+	req, err := http.NewRequest(http.MethodGet, c.baseURL.ResolveReference(p).String(), nil)
+	if err != nil {
+		fmt.Println("must be an errors at req")
+		return nil, err
+	}
+
+	cookie := http.Cookie{Name: "token", Value: c.accessToken}
+	req.AddCookie(&cookie)
+
+	resp, err := c.DoRequest(req)
+	if err != nil {
+		return nil, fmt.Errorf("error making http call: %w", err)
+	}
+
+	var r *Records
+	err = json.NewDecoder(resp.Body).Decode(&r)
+	if err != nil {
+		return nil, fmt.Errorf("GetRecords error decoding response: %w", err)
+	}
+
+	return r, err
+}
+
+func (c *AlkatronicClient) GetLastResult(deviceID int) (Record, error) {
+	records, err := c.GetRecords(deviceID, 7)
+
+	var dates []int64
+	for _, v := range records.Data {
+		dates = append(dates, v.CreateTime)
+	}
+
+	var latest int64 = 0
+	for _, v := range dates {
+		if latest < v {
+			latest = v
+		}
+	}
+
+	r := Record{}
+	for _, record := range records.Data {
+		if record.CreateTime == latest {
+			r = record
+		}
+	}
+
+	return r, err
+}
+
+// ConvertKh takes in the reported KH value and converts to dKh
+func ConvertKh(kh int) float32 {
+	return float32(kh) / 100.0
+}
---
+++ api/types.go
@@ -0,0 +1,112 @@
+package api
+
+type Response struct {
+	Result  bool   `json:"result"`
+	Message string `json:"message"`
+	Data    string `json:"data"`
+}
+
+type Devices struct {
+	Result  bool   `json:"result"`
+	Message string `json:"message"`
+	Data    []struct {
+		DeviceID                               int         `json:"device_id"`
+		SerialNumber                           string      `json:"serial_number"`
+		BatchNo                                int         `json:"batch_no"`
+		FirmwareVersion                        string      `json:"firmware_version"`
+		NextFirmwareVersion                    interface{} `json:"next_firmware_version"`
+		NextTestTime                           int         `json:"next_test_time"`
+		UserID                                 int         `json:"user_id"`
+		FriendlyName                           string      `json:"friendly_name"`
+		UpperKh                                int         `json:"upper_kh"`
+		LowerKh                                int         `json:"lower_kh"`
+		MeasureInterval                        int         `json:"measure_interval"`
+		AquariumVolume                         int         `json:"aquarium_volume"`
+		ReagentAlert                           int         `json:"reagent_alert"`
+		DoseBufferAlert                        int         `json:"dose_buffer_alert"`
+		KhAlert                                int         `json:"kh_alert"`
+		BaselineCalibration                    int         `json:"baseline_calibration"`
+		IsAutoUpdate                           int         `json:"is_auto_update"`
+		IsActionModeOn                         int         `json:"is_action_mode_on"`
+		IsWashoutModeOn                        int         `json:"is_washout_mode_on"`
+		IsDosetronicModeOn                     int         `json:"is_dosetronic_mode_on"`
+		IsFastModeOn                           int         `json:"is_fast_mode_on"`
+		CorrectionMethod                       int         `json:"correction_method"`
+		RetestDuration                         int         `json:"retest_duration"`
+		SettingsUpdateTime                     int         `json:"settings_update_time"`
+		LastCalibrateA                         int         `json:"last_calibrate_a"`
+		LastCalibrateC                         int         `json:"last_calibrate_c"`
+		LastCalibrateD                         int         `json:"last_calibrate_d"`
+		LastCalibrateP                         int         `json:"last_calibrate_p"`
+		LastOnline                             int         `json:"last_online"`
+		IsActive                               int         `json:"is_active"`
+		IsBetaTest                             bool        `json:"is_beta_test"`
+		TimeZone                               interface{} `json:"time_zone"`
+		LocalIPAddress                         interface{} `json:"local_ip_address"`
+		MonitorBy                              interface{} `json:"monitor_by"`
+		UnlinkToken                            interface{} `json:"unlink_token"`
+		CreateTime                             int         `json:"create_time"`
+		UpdateTime                             int         `json:"update_time"`
+		IsAllowCloudSettings                   bool        `json:"is_allow_cloud_settings"`
+		IsAllowDosetronicSettings              bool        `json:"is_allow_dosetronic_settings"`
+		IsAllowKBncSettings                    bool        `json:"is_allow_k_bnc_settings"`
+		IsAllowMtestPhtestRefillSettings       bool        `json:"is_allow_mtest_phtest_refill_settings"`
+		IsAllowSdaOdaRetestSettings            bool        `json:"is_allow_sda_oda_retest_settings"`
+		IsAllowPlugSettings                    bool        `json:"is_allow_plug_settings"`
+		IsUseRecordFormatV2                    bool        `json:"is_use_record_format_v2"`
+		IsAllowResetPhProbeCalibrationSettings bool        `json:"is_allow_reset_ph_probe_calibration_settings"`
+		IsAllowSuperLowReferenceSettings       bool        `json:"is_allow_super_low_reference_settings"`
+	} `json:"data"`
+}
+
+type Records struct {
+	Result  bool   `json:"result"`
+	Message string `json:"message"`
+	Data    []struct {
+		RecordID            int         `json:"record_id"`
+		DeviceID            int         `json:"device_id"`
+		KhValue             int         `json:"kh_value"`
+		PhValue             int         `json:"ph_value"`
+		SolutionAdded       int         `json:"solution_added"`
+		AcidUsed            int         `json:"acid_used"`
+		IsPowerPlugOn       int         `json:"is_power_plug_on"`
+		Indicator           int         `json:"indicator"`
+		RemainingReagent    int         `json:"remaining_reagent"`
+		RemainingDoseBuffer int         `json:"remaining_dose_buffer"`
+		TestCount           int         `json:"test_count"`
+		IsHidden            int         `json:"is_hidden"`
+		Note                interface{} `json:"note"`
+		IsDeleted           int         `json:"is_deleted"`
+		LocalHour           interface{} `json:"local_hour"`
+		LocalMinute         interface{} `json:"local_minute"`
+		RecordTime          int         `json:"record_time"`
+		CreateTime          int64       `json:"create_time"`
+		UpdateTime          int         `json:"update_time"`
+		Db1                 interface{} `json:"db1"`
+		HumanRecordTime     string      `json:"human_record_time"`
+	} `json:"data"`
+}
+
+type Record struct {
+	RecordID            int         `json:"record_id"`
+	DeviceID            int         `json:"device_id"`
+	KhValue             int         `json:"kh_value"`
+	PhValue             int         `json:"ph_value"`
+	SolutionAdded       int         `json:"solution_added"`
+	AcidUsed            int         `json:"acid_used"`
+	IsPowerPlugOn       int         `json:"is_power_plug_on"`
+	Indicator           int         `json:"indicator"`
+	RemainingReagent    int         `json:"remaining_reagent"`
+	RemainingDoseBuffer int         `json:"remaining_dose_buffer"`
+	TestCount           int         `json:"test_count"`
+	IsHidden            int         `json:"is_hidden"`
+	Note                interface{} `json:"note"`
+	IsDeleted           int         `json:"is_deleted"`
+	LocalHour           interface{} `json:"local_hour"`
+	LocalMinute         interface{} `json:"local_minute"`
+	RecordTime          int         `json:"record_time"`
+	CreateTime          int64       `json:"create_time"`
+	UpdateTime          int         `json:"update_time"`
+	Db1                 interface{} `json:"db1"`
+	HumanRecordTime     string      `json:"human_record_time"`
+}
